f49462e4f8a3545fd459632ae472d4ea48504d1b
Ed Baskerville <ed@edbaskerville.com>
2021-04-09 16:32:13 -0700

    Added docstrings for continuous/output.jl. Modified host sampling to be without replacement.

--- src/continuous/output.jl
+++ src/continuous/output.jl
@@ -1,10 +1,19 @@
+"""
+Output functions for exact continuous-time model.
+"""
+
+"""
+Write output to `summary` table
+"""
 function write_summary(db, t, s, stats)
 #     println("write_summary($(t))")
     
+    # Compute number of infections (liver, active, and both) with a simple tally/sum.
     n_infections_liver = sum(length(host.liver_infections) for host in s.hosts)
     n_infections_active = sum(length(host.active_infections) for host in s.hosts)
     n_infections = n_infections_liver + n_infections_active
     
+    # Compute number of individuals with infections (liver, active, or either).
     n_infected_liver = sum(length(host.liver_infections) > 0 for host in s.hosts)
     n_infected_active = sum(length(host.active_infections) > 0 for host in s.hosts)
     n_infected = sum(
@@ -12,9 +21,11 @@ function write_summary(db, t, s, stats)
         for host in s.hosts
     )
     
+    # Compute elapsed time in seconds.
     next_datetime = now()
     exec_time = Dates.value(next_datetime - stats.start_datetime) / 1000.0
     
+    # Write to summary table.
     execute(db.summary, (
         t,
         n_infections_liver,
@@ -31,14 +42,21 @@ function write_summary(db, t, s, stats)
         exec_time
     ))
     
+    # Reset counters and elapsed time.
     reset!(stats, next_datetime)
 end
 
+"""
+Write output for periodically sampled hosts.
+"""
 function write_host_samples(db, t, s)
 #     println("write_host_samples($(t))")
     
-    for i in 1:P.host_sample_size
-        host = rand(s.hosts)
+    # Sample `host_sample_size` hosts randomly (without replacement).
+    hosts = sample(s.hosts, P.host_sample_size, replace = false)
+    
+    # For each host, write out birth/death time and each infection.
+    for host in hosts
         execute(db.sampled_hosts, (t, Int64(host.id), host.t_birth, host.t_death))
         
         for infection in host.liver_infections
@@ -51,6 +69,20 @@ function write_host_samples(db, t, s)
     end
 end
 
+"""
+Write output for a single infection from a sampled host.
+
+Infections are written to the `sampled_infections` table, and the genes for the
+infections are written to `sampled_infection_genes`.
+
+`expression_index` indicates which gene is currently expressed. Liver-stage
+infections are indicated using a SQLite `NULL` (Julia `missing`) for
+`expression_index`.
+
+For each infection, the `sampled_infection_genes` table contains one row for
+each expression index, with the final columns containing the allele IDs for each
+locus.
+"""
 function write_infection(db, t, host, infection)
 #     println("write_infection($(t), $(host.id), $(infection.id))")
     execute(
@@ -65,6 +97,13 @@ function write_infection(db, t, host, infection)
     end
 end
 
+"""
+Write gene and strain counts to the `gene_strain_counts` table.
+
+Counts are not maintained dynamically during the simulation; this function
+simply scans all host infections and assembles sets of all genes and all
+strains.
+"""
 function write_gene_strain_counts(db, t, s)
 #     println("write_gene_strain_counts($(t))")
     genes::Set{Gene} = Set()
@@ -78,6 +117,12 @@ function write_gene_strain_counts(db, t, s)
     execute(db.gene_strain_counts, (t, length(genes), length(strains)))
 end
 
+"""
+Count genes and strains for a particular list of infections in a particular host.
+
+This function simply adds genes and strains from each infection to corresponding
+sets.
+"""
 function count_genes_and_strains!(genes, strains, infections)
 #     println("count_circulating_genes_and_strains()")
     for infection in infections
