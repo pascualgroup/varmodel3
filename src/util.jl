ddf4faf3f500b4d215d160775908b8422bc5b55f
Ed Baskerville <ed@edbaskerville.com>
2021-06-13 14:00:15 -0700

    Removed unused functions and copied new comments from varmodel4

--- src/util.jl
+++ src/util.jl
@@ -9,53 +9,29 @@ import Base.in
 
 using Distributions
 
-function fill_mask_with_entries(mask, entries::Union{AbstractArray{T}, AbstractRange{T}}) where {T}
-    x = fill(T(0), size(mask))
-    x[mask] = entries
-    x
-end
+"""
+Samples an index using weights.
 
-function mask_with_column_limits(mask, limits)
-#     println("mask = $(mask), limits = $(limits)")
-    @assert length(limits) == size(mask)[2]
-    count = fill(0, length(limits))
-    new_mask = falses(size(mask))
-    for i in 1:size(mask)[1]
-        new_mask[i,:] = mask[i,:] .& (count .< limits)
-        count[:] .+= new_mask[i,:]
-    end
-#     println("new_mask = $(new_mask)")
-    new_mask
-end
+Assumes that `total_weight == sum(weights)`.
 
-function sample_true_indices_by_column(mask)
-    @assert all(sum(mask; dims = 1) .> 0)
+The sampling algorithm can be visualized by arranging boxes left to right,
+with widths equal to the weights, in sequence, numbered `1:length(weights)`;
+the leftmost box has its left edge at 0.
 
-    n, m = size(mask)
-    row_indices = fill(0, m)
-    col_indices = collect(1:m)
-    while length(col_indices) > 0
-        new_row_indices = rand(1:n, length(col_indices))
-        valid_row_indices = mask[zip_cartesian(new_row_indices, col_indices)]
-        row_indices[col_indices] .= new_row_indices .* valid_row_indices
-        col_indices = col_indices[.!valid_row_indices]
-    end
+```
+|----------------------------------------------------|
+| weights[1] |     weights[2]     |    weights[3]    |
+|-----------------|----------------------------------|
+0           sampled_location                    total_weight
+```
 
-    row_indices
-end
-
-function findfirst_each_column(x)
-    inds = fill(0, size(x)[2])
-    for j in 1:size(x)[2]
-        inds[j] = findfirst(x[:,j])
-    end
-    inds
-end
-
-function zip_cartesian(x...)
-    collect(CartesianIndex(x) for x in zip(x...))
-end
-
+To sample, draw a real number is uniformly randomly in [0, total_weight];
+this sampled location corresponds to a point within one of the boxes.
+To identify which box, scan the boxes left to right while computing the
+cumulative sum.
+If the sampled location is less than the cumulative sum, return the index of
+the most recently scanned box.
+"""
 function direct_sample_linear_scan(weights, total_weight)
     bin_right_side = 0.0
     sampled_location = rand() * total_weight
@@ -68,6 +44,9 @@ function direct_sample_linear_scan(weights, total_weight)
     length(weights)
 end
 
+"""
+Copies the columns of `src` to `dst` in random order.
+"""
 function shuffle_columns_to!(dst, src)
     m = size(dst)[2]
     src_indices = MVector{m, Int}(1:m)
@@ -75,6 +54,9 @@ function shuffle_columns_to!(dst, src)
     dst[:,:] = src[:,src_indices]
 end
 
+"""
+Copies a random sample of the columns of `src1` and `src2` to `dst`.
+"""
 function sample_columns_from_two_matrices_to!(dst, src1, src2)
     m_dst = size(dst)[2]
     m_src_1 = size(src1)[2]
@@ -93,6 +75,12 @@ function sample_columns_from_two_matrices_to!(dst, src1, src2)
     nothing
 end
 
+"""
+Deletes index `i` in the array and replaces it with the last entry in the array.
+
+This enables maintaining a set of objects in an array (in arbitrary order)
+so they can be uniformly randomly sampled, with constant-time cost for removal.
+"""
 function delete_and_swap_with_end!(a, i)
     @assert i <= length(a)
 
@@ -103,6 +91,16 @@ function delete_and_swap_with_end!(a, i)
     nothing
 end
 
+"""
+Returns a key in a dictionary, by iteration order.
+
+Dictionary iteration order is undefined, but if `index` has been uniformly
+randomly sampled, this will have the effect of uniformly randomly sampling
+a key in the dictionary.
+
+This is used to avoid the extra memory expense of maintaining a randomly
+samplable array.
+"""
 function get_key_by_iteration_order(d::Dict{K, V}, index::Int) where {K, V}
     for (i, k) in enumerate(keys(d))
         if i == index
