8eaed491dae63a09cf44be6386a0a00830167fab
Frederic Labbe <frederic.labbe.phd@gmail.com>
2023-03-03 17:26:25 +0400

    Update calc_targets.py
    PTS per host (instead of per strain)

--- emews/python/calc_targets.py
+++ emews/python/calc_targets.py
@@ -17,6 +17,8 @@ import pandas as pd
 import argparse
 import random
 import numpy as np
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 pd.options.mode.chained_assignment = None
 
 
@@ -74,7 +76,8 @@ def calc_targets(input_file, time, measurement):
                 subsamp.extend(var_samp)
                 genes.extend(var_samp)
                 subset = pd.DataFrame(var_samp, columns = ['gene_id'])
-                subset['strain_id'] = np.repeat(var_strain.strain_id.unique(), len(subset))            
+                subset['strain_id'] = np.repeat(var_strain.strain_id.unique(), len(subset))
+                subset['host_id'] = np.repeat(host, len(subset))
                 dfs.append(subset)
             nb_var_err = len(set(subsamp))
             MOI = 1
@@ -147,7 +150,7 @@ def Prevalence(df1, df2):
 def PTS(df):
     if len(df['strain_id'].unique()) > 1000:
         df = df.sample(n = 1000)
-    g = df.groupby('strain_id')['gene_id'].apply(list).reset_index()
+    g = df.groupby('host_id')['gene_id'].apply(list).reset_index()
     genemat = g.join(pd.get_dummies(g['gene_id'].apply(pd.Series).stack()).sum(level = 0)).drop('gene_id', 1)
     genemat = genemat.iloc[: , 1:]
     genemat = genemat.to_numpy()
