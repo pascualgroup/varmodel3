099b105f052ea1073bae971861b74a9bc66af000
qzhan321 <qizhan@uchicago.edu>
2024-03-14 15:25:43 -0400

    remove the immunity_level_max parameter

--- src/parameters.jl
+++ src/parameters.jl
@@ -226,7 +226,7 @@ keyword constructor for the class.
 
     See description in the `immunity` field of struct `State`.
     """
-    immunity_level_max::Union{Int16, Nothing} = nothing
+    # immunity_level_max::Union{Int16, Nothing} = nothing
 
     """
     Rate at which immunity is lost, per host, per gene.
