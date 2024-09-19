30d0bceb05292558d3e10a5e4268ff3d5fe262fe
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-09-19 18:20:14 -0500

    Update model.jl

--- src/model.jl
+++ src/model.jl
@@ -43,14 +43,14 @@ for i in 1:(P.n_genes_per_strain - 1)
 end
 
 num_genes_var_groups = []
-for i in 1:length(P.var_groups_ratio)
-    num_genes_var_group = round(Int, P.var_groups_ratio[i] * P.n_genes_initial) 
+for i in 1:length(P.var_groups_ratio_regional_pool)
+    num_genes_var_group = round(Int, P.var_groups_ratio_regional_pool[i] * P.n_genes_initial) 
     push!(num_genes_var_groups, num_genes_var_group)
 end
 
 allele_ids_var_groups = []
-num_allele_ids_var_groups = round.(Int, P.var_groups_ratio * P.n_alleles_per_locus_initial)
-for i in 1:length(P.var_groups_ratio)
+num_allele_ids_var_groups = round.(Int, P.var_groups_ratio_regional_pool * P.n_alleles_per_locus_initial)
+for i in 1:length(P.var_groups_ratio_regional_pool)
     allele_ids_var_group = if i == 1
         1:num_allele_ids_var_groups[i]
     else 
@@ -257,7 +257,7 @@ function initialize_state(rng)
     # are unique randomly generated genes. Initialize gene-to-group-id map as an empty dictionary.
     gene_pool_set = Set()
     association_genes_to_var_groups_init = Dict{Gene, GeneGroupId}()
-    for group_id in 1:length(P.var_groups_ratio) # Create genes for each group
+    for group_id in 1:length(P.var_groups_ratio_regional_pool) # Create genes for each group
         num_genes_var_group = num_genes_var_groups[group_id] 
         if num_genes_var_group > 0
             counter = sum(num_genes_var_groups[1:group_id])
