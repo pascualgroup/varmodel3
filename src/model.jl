dbcf79541da2b15ff64be803dd4d8e0f26df57ab
qzhan321 <42386885+qzhan321@users.noreply.github.com>
2024-10-10 11:37:46 -0400

    Update model.jl

--- src/model.jl
+++ src/model.jl
@@ -387,7 +387,7 @@ function initialize_genes_from_pool(rng, gene_pool, infection, association_genes
             end
         end
     else
-        gene_indices = rand(rng, 1:size(s.gene_pool)[2], P.n_genes_per_strain)
+        gene_indices = rand(rng, 1:size(gene_pool)[2], P.n_genes_per_strain)
         infection.genes[:,:] = (@view gene_pool[:, gene_indices]),
         for i in 1:size(infection.genes)[2]
             group_id = association_genes_to_var_groups[infection.genes[:,i]]
