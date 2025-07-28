37a0b2e93d9233c0cda01ffc78f3bc53e5468886
Qi Zhan <qzhan0507@gmail.com>
2025-07-28 15:09:10 -0400

    Specify the proportion instead of a fixed number of host sampled because the population size is allowed to increase.

--- src/output.jl
+++ src/output.jl
@@ -272,8 +272,8 @@ function write_output!(db, t, s, stats)
         end
         
         if (P.calc_targets !== nothing && P.calc_targets && t in P.calc_targets_times) || (P.output_host_samples !== nothing && P.output_host_samples && t in P.host_sampling_times)
-            # Sample `host_sample_size` hosts randomly (without replacement).
-            sampled_hosts = sample(s.hosts, P.host_sample_size, replace = false)
+            # Sample `host_sample_proportion * n_hosts` hosts randomly (without replacement).
+            sampled_hosts = sample(s.hosts, floor(P.host_sample_proportion * P.n_hosts), replace = false)
         
             if P.calc_targets !== nothing && P.calc_targets && t in P.calc_targets_times
                 for threshold in P.thresholds
